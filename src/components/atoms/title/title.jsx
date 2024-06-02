15e769786ccfa60696cfa7d419c19c1c76bc662c
Philippe Beck <philippe@philippebeck.net>
2024-06-02 16:50:40 +0400

    Improve title

--- src/components/atoms/title/title.jsx
+++ src/components/atoms/title/title.jsx
@@ -1,16 +1,14 @@
 import "./title.scss"
 
-const Title = ({ title, lvl="2", subtitle="" }) => {
-
+const Title = ({ title, lvl="", subtitle="" }) => {
   return (
+
     <hgroup>
       {
         lvl === "1" ? <h1>{title}</h1> :
         lvl === "2" ? <h2>{title}</h2> :
         lvl === "3" ? <h3>{title}</h3> :
-        lvl === "4" ? <h4>{title}</h4> :
-        lvl === "5" ? <h5>{title}</h5> :
-        <h6>{title}</h6> 
+        <h4>{title}</h4>
       }
       {
         subtitle && <p>{subtitle}</p>
