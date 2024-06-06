06c92d62b7c174b19125989faffad3842a9a3df8
Philippe Beck <philippe@philippebeck.net>
2024-06-06 09:57:17 +0400

    Add JSDoc to Title

--- src/components/molecules/title/title.jsx
+++ src/components/molecules/title/title.jsx
@@ -2,7 +2,34 @@ import Icon from '../../atoms/icon/icon'
 
 import "./title.scss"
 
-const Title = ({ title, lvl="2", cat="brands", icon="", subtitle="" }) => {
+/**
+ * ? TITLE COMPONENT
+ * * Renders a title component with the specified
+ * * title, level, category, icon & subtitle.
+ *
+ * @param {Object} props
+ *  The properties for the Title component.
+ *
+ * @param {string} props.title
+ *  The title of the component.
+ *
+ * @param {string} [props.lvl="2"]
+ *  The level of the title (1, 2, 3, or 4).
+ *  Defaults to 2.
+ *
+ * @param {string} props.cat
+ *  The category of the title.
+ *
+ * @param {string} [props.icon=""]
+ *  The icon for the title.
+ *
+ * @param {string} [props.subtitle=""]
+ *  The subtitle for the title.
+ *
+ * @return {JSX.Element}
+ *  The rendered title component.
+ */
+const Title = ({ title, lvl="2", cat, icon="", subtitle="" }) => {
   return (
 
     <hgroup>
