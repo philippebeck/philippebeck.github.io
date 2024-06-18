fe46ceebb9c16e4479491639c22656d865b9ee64
Philippe Beck <philippe@philippebeck.net>
2024-06-18 12:36:00 +0400

    Improve Title to be more dynamic & move it

--- src/components/atoms/title/title.jsx
+++ src/components/atoms/title/title.jsx
@@ -1,11 +1,9 @@
-import Icon from '../../atoms/icon/icon'
-
 import "./title.scss"
 
 /**
  * ? TITLE COMPONENT
  * * Renders a title component with the specified
- * * heading, icon & subtitle.
+ * * heading, prefix & subtitle.
  *
  * @param {Object} props
  *  The properties for the Title component.
@@ -17,26 +15,21 @@ import "./title.scss"
  * @param {string} props.heading.text
  *  The text of the heading.
  *
- * @param {Object} props.icon
- *  The icon object for the title.
- * @param {string} props.icon.name
- *  The name of the icon.
- * @param {string} props.icon.cat
- *  The category of the icon.
- *
- * @param {string} [props.subtitle=""]
- *  The subtitle for the title.
+ * @param {any} [props.prefix=""]
+ *  The prefix of the title.
+ * @param {any} [props.subtitle=""]
+ *  The subtitle of the title.
  *
  * @return {JSX.Element}
  *  The rendered title component.
  */
-const Title = ({ heading, icon, subtitle="" }) => {
+const Title = ({ heading, prefix="", subtitle="" }) => {
   return (
 
     <hgroup>
-      <p>
-        <Icon name={icon.name} cat={icon.cat} />
-      </p>
+      {
+        prefix && <p>{prefix}</p>
+      }
       {
         heading.level === "1" ? <h1>{heading.text}</h1> :
         heading.level === "2" ? <h2>{heading.text}</h2> :
