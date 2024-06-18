10edad2da8bfa967449e2eb7d628ca35adad1bc2
Philippe Beck <philippe@philippebeck.net>
2024-06-18 12:36:51 +0400

    Add about to main & update import for navbar & footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import Navbar from './components/organisms/navbar/navbar'
+import Navbar from './components/molecules/navbar/navbar'
+import Footer from './components/molecules/footer/footer'
+
 import Header from './components/organisms/header/header'
 import Blog from './components/organisms/blog/blog'
 import Portfolio from './components/organisms/portfolio/portfolio'
 import Links from './components/organisms/links/links'
-import Footer from './components/organisms/footer/footer'
+import About from './components/organisms/about/about'
 
-import './assets/app.scss'
 import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+
     <Header />
     <Portfolio />
     <Blog />
     <Links />
+    <About />
+
     <Footer />
   </React.StrictMode>
 )
