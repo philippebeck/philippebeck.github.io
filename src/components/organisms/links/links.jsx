6e744d3665549063973a2b9a0550be06e8860f0b
Philippe Beck <philippe@philippebeck.net>
2024-06-21 09:26:28 +0400

    Update List usage to Links by adding Link

--- src/components/organisms/links/links.jsx
+++ src/components/organisms/links/links.jsx
@@ -1,10 +1,11 @@
 import { links } from '../../../assets/data.json'
 
 import Icon from '../../atoms/icon/icon'
+import Link from '../../atoms/link/link'
+import List from '../../atoms/list/list'
 import Title from '../../atoms/title/title'
 
 import Collapse from '../../molecules/collapse/collapse'
-import List from '../../molecules/list/list'
 
 import './links.scss'
 
@@ -40,7 +41,16 @@ const Links = () => {
               title={link.title}
               content={
 
-                <List array={link.list} />
+                <List
+                  list={link.list.map((item, i) =>
+
+                  <Link
+                    key={i}
+                    url={item.url}
+                    content={item.name}
+                    design="btn"
+                  />
+                )}/>
               }
             />
           </li>
