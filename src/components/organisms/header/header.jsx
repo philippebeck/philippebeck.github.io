92e365507c5750b0236a2bd7ac0bc745c502609d
Philippe Beck <philippe@philippebeck.net>
2024-06-29 22:12:51 +0400

    Update header with new molecules & move it to organisms folder

---
+++ src/components/organisms/header/header.jsx
@@ -0,0 +1,32 @@
+import { header } from '../../../assets/data.json'
+
+import Heading from '../../molecules/heading/heading'
+import List from '../../atoms/list/list'
+
+import './header.scss'
+
+/**
+ * ? HEADER ORGANISMS
+ * * Renders the Header Organisms
+ * * that displays the main information
+ *
+ * @return {JSX.Element}
+ *  The rendered Header Organisms.
+ */
+const Header = () => {
+  const { title, data } = header;
+
+  return (
+    <header id="header">
+      <Heading title={title} />
+
+      <List
+        array={data.map((item) =>
+          <>{item}</>
+        )}
+      />
+    </header>
+  )
+}
+
+export default Header
