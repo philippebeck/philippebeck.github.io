82233082063e98ec106170b7a602625c2f1f7f75
Philippe Beck <philippe@philippebeck.net>
2024-06-29 22:12:11 +0400

    Update navbar with new molecules & move it to organisms folder

---
+++ src/components/organisms/navbar/navbar.jsx
@@ -0,0 +1,34 @@
+import { nav } from '../../../assets/data.json'
+
+import Button from '../../molecules/button/button'
+import List from '../../atoms/list/list'
+
+import './navbar.scss'
+
+/**
+ * ? NAVBAR ORGANISMS
+ * * Renders the Navbar Organisms
+ * * that displays the navigation links
+ *
+ * @return {JSX.Element}
+ *  The rendered Navbar Organisms.
+ */
+const Navbar = () => {
+  const { logo, links, contact } = nav;
+
+  return (
+    <nav className="navbar">
+      <Button link={logo} />
+
+      <List
+        array={links.map((item) =>
+          <Button link={item} />
+        )}
+      />
+
+      <Button link={contact} />
+    </nav>
+  )
+}
+
+export default Navbar
