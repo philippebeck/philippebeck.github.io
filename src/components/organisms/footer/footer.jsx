600a279b7e6e8133a37697379b28aff6da2c85ad
Philippe Beck <philippe@philippebeck.net>
2024-06-29 22:14:26 +0400

    Update footer with new molecules & move it to organisms folder

---
+++ src/components/organisms/footer/footer.jsx
@@ -0,0 +1,29 @@
+import { footer } from '../../../assets/data.json'
+
+import Button from '../../molecules/button/button'
+import List from '../../atoms/list/list'
+
+import './footer.scss'
+
+/**
+ * ? FOOTER ORGANISMS
+ * * Renders the Footer Organisms
+ * * that displays a list of social links
+ *
+ * @return {JSX.Element}
+ *  The rendered Footer Organisms.
+ */
+const Footer = () => {
+
+  return (
+    <footer className="footer">
+      <List
+        array={footer.map((item) =>
+          <Button link={item} />
+        )}
+      />
+    </footer>
+  )
+}
+
+export default Footer
