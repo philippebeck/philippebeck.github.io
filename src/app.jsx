817f0e9e75dcf931afd99a308ec42c9d021226d5
Philippe Beck <philippebeck.dev@gmail.com>
2024-09-20 17:52:00 +0200

    Add Navbar & Footer from reactoms

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './components/navbar/navbar'
+import { Navbar, Footer } from 'reactoms'
+
 import Header from './components/header/header'
 import Portfolio from './components/portfolio/portfolio'
 import Stats from './components/stats/stats'
 import Blog from './components/blog/blog'
 import About from './components/about/about'
 import Tools from './components/tools/tools'
-import Footer from './components/footer/footer'
+
+import data from './assets/data.json'
 import './assets/style/app.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
+    <Navbar array={data.nav}/>
+
     <Header />
     <Portfolio />
     <Stats />
     <Blog />
     <About />
     <Tools />
-    <Footer />
+
+    <Footer array={data.footer}/>
   </React.StrictMode>
 )
